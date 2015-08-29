998eec57e2c4fbcacfc655a2761f87c09891b1bb
Jay Hayes <ur@iamvery.com>
2015-08-29 08:35:13 -0500

    Refactor app list, include hound

--- mix.exs
+++ mix.exs
@@ -17,9 +17,15 @@ defmodule HelloPhoenix.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [mod: {HelloPhoenix, []},
-     applications: [:phoenix, :phoenix_html, :cowboy, :logger,
-                    :phoenix_ecto, :postgrex]]
+     applications: app_list(Mix.env) ]
   end
+
+  def app_list do
+    [:phoenix, :phoenix_html, :cowboy, :logger, :phoenix_ecto, :postgrex]
+  end
+
+  def app_list(:test), do: [:hound | app_list]
+  def app_list(_),     do: app_list
 
   # Specifies which paths to compile per environment
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
