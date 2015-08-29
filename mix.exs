1f4bc4de91298ff50572fd370795a002fdfaec67
Jay Hayes <ur@iamvery.com>
2015-08-29 08:34:49 -0500

    Install hound
    * browser-driven tests

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule HelloPhoenix.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:hound, "~> 0.7", only: :test}]
   end
 end
